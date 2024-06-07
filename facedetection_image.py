063ec0ba55c6c9d851d9b1ad6af6fb9edba7d6f0
girijageddavalasa <148637003+girijageddavalasa@users.noreply.github.com>
2024-06-07 15:39:11 +0530

    Add files via upload

---
+++ facedetection_image.py
@@ -0,0 +1,22 @@
+import cv2
+alg="haarcascade_frontalface_default.xml"
+haar_cascade=cv2.CascadeClassifier(alg) #load algorthim
+image="happy.jpg"
+img=cv2.imread(image)
+while True:
+    grayimg=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    face_coord=haar_cascade.detectMultiScale(grayimg,1.3,10)
+    for (x,y,w,h) in face_coord:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+    if len(face_coord)==0:
+        text="face not detected"
+    else:
+        text="face detected"
+    cv2.putText(img,text,(10,20),cv2.FONT_HERSHEY_SIMPLEX,1,(10,150,20),2)
+    cv2.imshow("FACE DETECTION",img)
+    key=cv2.waitKey(10)
+    if key==27:
+        break
+cam.release()
+cv2.destroyAllWindows()
+            
